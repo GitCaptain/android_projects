194977255812d60613ec496a68ae3a0e6c956f32
Sukhikh Alexander <xbox24_772@mail.ru>
2019-07-29 18:52:14 +0700

    Update MyMessanger

--- MyMessenger/app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
+++ MyMessenger/app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
@@ -6,10 +6,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-public class CreateMessageActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateMessageActivity extends AppCompatActivity {
 
     EditText text;
-    static final String EXTRA_MESSAGE = "EXTRA";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +20,10 @@ public class CreateMessageActivity extends Activity {
     }
 
     public void onSendMessage(View view){
-        Intent intent = new Intent(this, ReceiveMessageActivity.class);
-        intent.putExtra(EXTRA_MESSAGE, text.getText().toString());
-        startActivity(intent);
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, text.getText().toString());
+        startActivity(Intent.createChooser(intent,  getString(R.string.chooser)));
+
     }
 }
--- MyMessenger/app/src/main/java/com/hfad/mymessenger/ReceiveMessageActivity.java
+++ MyMessenger/app/src/main/java/com/hfad/mymessenger/ReceiveMessageActivity.java
@@ -13,7 +13,7 @@ public class ReceiveMessageActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_message);
-        text = getIntent().getStringExtra(CreateMessageActivity.EXTRA_MESSAGE);
+        text = "";//getIntent().getStringExtra(CreateMessageActivity.EXTRA_MESSAGE);
         textView = (TextView) findViewById(R.id.set_message);
         textView.setText(text);
     }
