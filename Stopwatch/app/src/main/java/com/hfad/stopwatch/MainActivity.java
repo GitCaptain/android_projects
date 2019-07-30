d06efd8815875071d7d0a0c3dfdfb9bd0813645a
Sukhikh Alexander <xbox24_772@mail.ru>
2019-07-30 20:15:44 +0700

    Project done. Timer works.

--- Stopwatch/app/src/main/java/com/hfad/stopwatch/MainActivity.java
+++ Stopwatch/app/src/main/java/com/hfad/stopwatch/MainActivity.java
@@ -2,6 +2,7 @@ package com.hfad.stopwatch;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.TextView;
 
@@ -11,22 +12,59 @@ import java.util.Locale;
 public class MainActivity extends Activity {
 
     TextView timer;
-    private boolean running = false;
+    private boolean running = false, wasRunning = false;
     private int seconds = 0;
+
+    private final String
+            RUNNING = "running",
+            SECONDS = "seconds",
+            WAS_RUNNING = "was running";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         timer = (TextView) findViewById(R.id.time_view);
+
+        if(savedInstanceState != null){
+            seconds = savedInstanceState.getInt(SECONDS);
+            running = savedInstanceState.getBoolean(RUNNING);
+            wasRunning = savedInstanceState.getBoolean(WAS_RUNNING);
+        }
+
+        timer.setText(getTime(seconds));
+        runTimer();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        savedInstanceState.putInt(SECONDS, seconds);
+        savedInstanceState.putBoolean(RUNNING, running);
+        savedInstanceState.putBoolean(WAS_RUNNING, wasRunning);
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        // when activity is not visible timer should not run
+        wasRunning = running;
+        running = false;
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        running = wasRunning;
     }
 
     public void onClickStart(View view){
         running = true;
+        wasRunning = true;
     }
 
     public void onClickStop(View view){
         running = false;
+        wasRunning = false;
     }
 
     public void onClickReset(View view){
@@ -35,15 +73,27 @@ public class MainActivity extends Activity {
     }
 
     private void runTimer(){
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run(){
 
-        int hours = seconds/3600;
-        int minutes = (seconds % 3600)/60;
-        int secs = seconds%60;
+                if(running)
+                    seconds++;
 
-        String time = String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, secs);
+                timer.setText(getTime(seconds));
 
-        if(running)
-            seconds++;
+                handler.postDelayed(this, 1000);
+            }
+        });
+    }
+
+    private String getTime(int seconds){
+        int hours = seconds / 3600;
+        int minutes = (seconds % 3600) / 60;
+        int secs = seconds % 60;
+
+        return String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, secs);
     }
 
 }
